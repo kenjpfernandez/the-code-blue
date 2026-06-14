2a97bcbc7a820a50d040dd4c51b9328ab2b8f82e
Kenneth Jay <93532281+kenjpfernandez@users.noreply.github.com>
2026-06-14 12:42:00 -0400

    Update game_schedule.js

--- docs/game_schedule.js
+++ docs/game_schedule.js
@@ -1,7 +1,5 @@
 const GAME_SCHEDULE = {
 
-  day2Unlock: new Date("2026-06-14T13:00:00"),
-
-  day3Unlock: new Date("2026-06-15T13:00:00")
-
+  day2Unlock: new Date("2026-06-18T11:00:00"),
+  day3Unlock: new Date("2026-06-19T11:00:00")
 };
